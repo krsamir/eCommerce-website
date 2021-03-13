8da0e7ca698d9416300e9e430a492e0a07a17665
Samir <samirkr2527@gmail.com>
2021-03-13 20:56:07 +0530

    HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
   return (
     <div className="">
       <Header />
       <main className="py-3">
         <Container>
-          <h2>Welocome to ProShop</h2>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
