e7c916ff89044e1d1491501710a83084d24bb73f
Samir <samirkr2527@gmail.com>
2021-03-14 21:15:03 +0530

    Fetching Product using axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,21 @@
-import React from "react";
-import products from "../products";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Product from "../Components/Product";
 import { Row, Col } from "react-bootstrap";
 const HomeScreen = () => {
+  const [prod, setProduct] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProduct(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {prod.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
           </Col>
