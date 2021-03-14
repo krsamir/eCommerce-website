41a2516d86fae08ce2f8331d0fa3661bc3608ff1
Samir <samirkr2527@gmail.com>
2021-03-14 20:29:10 +0530

    Setting up Backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express(),
+  log = console.log,
+  products = require("./data/products");
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+app.listen(5000, log("Server running on port 5000"));
