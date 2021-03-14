439b14ec77481bce07b77e312dbcfd3e807c714a
Samir <samirkr2527@gmail.com>
2021-03-14 09:42:27 +0530

    Rating Color Change

--- frontend/src/Components/Rating.js
+++ frontend/src/Components/Rating.js
@@ -69,7 +69,7 @@ const Rating = ({ value, text, color }) => {
   );
 };
 Rating.defaultProps = {
-  color: "#f8e825",
+  color: "#ffa41c",
 };
 Rating.propTypes = {
   value: PropTypes.number.isRequired,
